55e434e0c3506aad1eef7f404c3ad0d866729245
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-22 02:54:17 -0800

    Add logic for parsing the input string

--- js/calculatorController.js
+++ js/calculatorController.js
@@ -16,5 +16,32 @@ calculatorApp.controller('calculatorController', ['$scope', function($scope) {
 
     $scope.getValue = function(element) {
         $scope.selectedInput = element.currentTarget.value;
+        $scope.inputSequence += $scope.selectedInput;
+        console.log($scope.inputSequence);
+    };
+
+    $scope.computeInput = function() {
+        var consecutiveOperators = /([*+\-/.])\1+/g;
+        var consecutiveDecimalPoints = /[.][0-9]*[.]/g;
+        var startWithOperator = /^[*+\-/.]+/;
+        var endWithOperator = /[*+\-/.]$/;
+        var divideByZero = /(\/0)/g;
+        var hasConsecutiveOperators = consecutiveOperators.test($scope.inputSequence);
+        var isStartWithOperator = startWithOperator.test($scope.inputSequence);
+        var isEndWithOperators = endWithOperator.test($scope.inputSequence);
+        var isDivideByZero = divideByZero.test($scope.inputSequence);
+        var hasConsecutiveDecimalPoints = consecutiveDecimalPoints.test($scope.inputSequence);
+        if(hasConsecutiveOperators || isStartWithOperator || isEndWithOperators || hasConsecutiveDecimalPoints) {
+            $scope.result = 'Syntax Error';
+        } else if (isDivideByZero) {
+            $scope.result = 'Math Error';
+        } else {
+            var op = /[*+\-/]/;
+            var ops = /[*+\-/]/g;
+            var inputArray = $scope.inputSequence.split(op);
+            var operators = $scope.inputSequence.match(ops);
+            console.log(inputArray);
+            console.log(operators);
+        }
     };
 }]);
