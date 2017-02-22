c73d0c1108423929ab7bd5d3c9738a22de301efb
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-22 00:23:38 -0800

    Enable retrieve button value when clicking on it

--- js/calculatorController.js
+++ js/calculatorController.js
@@ -1,3 +1,20 @@
+"use strict";
+
 calculatorApp.controller('calculatorController', ['$scope', function($scope) {
     $scope.result = 0;
+    $scope.inputSequence = '';
+    $scope.selectedInput = '';
+
+    $scope.clearAll = function() {
+        $scope.result = 0;
+        $scope.inputSequence = '';
+    };
+
+    $scope.clearLast = function() {
+        $scope.inputSequence = $scope.inputSequence.slice(0, -1);
+    };
+
+    $scope.getValue = function(element) {
+        $scope.selectedInput = element.currentTarget.value;
+    };
 }]);
