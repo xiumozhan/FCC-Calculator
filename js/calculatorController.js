08d4459291776a0c3afd875e4c636d27db70268e
Xiumo Zhan <xiumozhan@gmail.com>
2017-02-22 19:18:33 -0800

    Keep adding functionality to calculator controller

--- js/calculatorController.js
+++ js/calculatorController.js
@@ -1,24 +1,46 @@
 "use strict";
 
 calculatorApp.controller('calculatorController', ['$scope', function($scope) {
-    $scope.result = 0;
+    var result = 0;
     $scope.inputSequence = '';
+    var inputSequence = '';
     $scope.selectedInput = '';
+    $scope.current = 0;
+    var op = /[*+\-/]/;
+    var ops = /[*+\-/]/g;
 
     $scope.clearAll = function() {
-        $scope.result = 0;
+        result = 0;
         $scope.inputSequence = '';
+        $scope.selectedInput = '';
+        $scope.current = 0;
+        inputSequence = '';
     };
 
     $scope.clearLast = function() {
         $scope.inputSequence = $scope.inputSequence.slice(0, -1);
+        inputSequence = inputSequence.slice(0, -1);
     };
 
     $scope.getValue = function(element) {
         $scope.selectedInput = element.currentTarget.value;
         $scope.inputSequence += $scope.selectedInput;
-        console.log($scope.inputSequence);
+        inputSequence += $scope.selectedInput;
+        // console.log($scope.inputSequence);
     };
+
+    $scope.$watch('inputSequence', function(newInput, oldInput) {
+        if(!op.test($scope.selectedInput)) {
+            if($scope.current === '0' || $scope.current === 'Syntax Error' || $scope.current === 'Math Error' || op.test(oldInput.slice(-1))) {
+                $scope.current = $scope.selectedInput;
+            } else {
+                $scope.current += $scope.selectedInput;
+            }
+        } else {
+            $scope.current = $scope.selectedInput;
+        }
+        console.log($scope.current);
+    });
 
     $scope.computeInput = function() {
         var consecutiveOperators = /([*+\-/.])\1+/g;
@@ -26,22 +48,29 @@ calculatorApp.controller('calculatorController', ['$scope', function($scope) {
         var startWithOperator = /^[*+\-/.]+/;
         var endWithOperator = /[*+\-/.]$/;
         var divideByZero = /(\/0)/g;
-        var hasConsecutiveOperators = consecutiveOperators.test($scope.inputSequence);
-        var isStartWithOperator = startWithOperator.test($scope.inputSequence);
-        var isEndWithOperators = endWithOperator.test($scope.inputSequence);
-        var isDivideByZero = divideByZero.test($scope.inputSequence);
-        var hasConsecutiveDecimalPoints = consecutiveDecimalPoints.test($scope.inputSequence);
+        var hasConsecutiveOperators = consecutiveOperators.test(inputSequence);
+        var isStartWithOperator = startWithOperator.test(inputSequence);
+        var isEndWithOperators = endWithOperator.test(inputSequence);
+        var isDivideByZero = divideByZero.test(inputSequence);
+        var hasConsecutiveDecimalPoints = consecutiveDecimalPoints.test(inputSequence);
         if(hasConsecutiveOperators || isStartWithOperator || isEndWithOperators || hasConsecutiveDecimalPoints) {
-            $scope.result = 'Syntax Error';
+            result = 'Syntax Error';
+            $scope.current = result;
+            result = 0;
+            inputSequence = '';
+            // $scope.selectedInput = '';
         } else if (isDivideByZero) {
-            $scope.result = 'Math Error';
+            result = 'Math Error';
+            $scope.current = result;
+            result = 0;
+            inputSequence = '';
+            // $scope.selectedInput = '';
         } else {
-            var op = /[*+\-/]/;
-            var ops = /[*+\-/]/g;
-            var inputArray = $scope.inputSequence.split(op);
-            var operators = $scope.inputSequence.match(ops);
+            var inputArray = inputSequence.split(op);
+            var operators = inputSequence.match(ops);
             console.log(inputArray);
             console.log(operators);
+
         }
     };
 }]);
